7b68991eb2f49a272d3d888e1974e976e83a1d7a
hamzsait <hamzasait@utexas.edu>
2021-05-23 21:17:58 -0500

    added blank search bar (will style later)

--- src/components/ResultFrame.js
+++ src/components/ResultFrame.js
@@ -1,11 +1,22 @@
 import React from "react";
 import {Table} from "react-bootstrap";
 import ResultList from "./ResultList";
+import { MDBCol, MDBIcon } from "mdbreact";
+import '../index.css'
 
 function ResultFrame(props) {
   return (
     <div>
-      <h1> Hello </h1>
+      <div id = "header">
+      <h1> Employee Directory </h1>
+      <MDBCol id = 'search' md="6">
+        <form className="form-inline mt-4 mb-4">
+          <MDBIcon icon="search" />
+          <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+        </form>
+      </MDBCol>
+      </div>
+      
       <Table striped bordered hover>
       <thead>
         <tr>

288a95ccd19762f0bde208c6b64a5525b0aa37b8
hamzsait <hamzasait@utexas.edu>
2021-05-24 23:00:07 -0500

    added an if statement to avoid crashing if props are empty

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 function ResultList(props) {
-  return (
-    props.value.value.map(result =>(
-        <tr key = {result.email}>
-          <th>{result.name.first} {result.name.last}</th>
-          <th>{result.email}</th>
-          <th><img alt = "User Missing" src = {result.picture.large}></img></th>
-        </tr>
-    ))
-  );
+  if (props.value.value){
+    return (
+      props.value.value.map(result =>(
+          <tr key = {result.email}>
+            <th>{result.name.first} {result.name.last}</th>
+            <th>{result.email}</th>
+            <th><img alt = "User Missing" src = {result.picture.large}></img></th>
+          </tr>
+      ))
+    ) 
+  }
+  else{
+    return null
+  }
+  
 }
 
 export default ResultList
